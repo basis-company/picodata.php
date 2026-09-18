0a32f45afcec59ff2fc9cd7f2579b5b9f87aa2d7
dmitry krokhin <nekufa@gmail.com>
2026-09-18 18:28:24 +0300

    add CI: php-cs-fixer style gate and github actions

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = (new Finder())
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder($finder);
